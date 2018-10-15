eccc429dbfdccf867df6f45083ac2f123321cb5f
honghen <honghen.15@gmail.com>
2018-10-15 12:27:16 +0800

    rest ful api 參考自yt的create restful api

---
+++ api/routes/products.js
@@ -0,0 +1,47 @@
+﻿const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+
+const checkAuth = require("../middleware/check-auth");
+const ProductController = require("../controllers/products");
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        // cb(null, "./uploads/");
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
+const Product = require("../models/product");
+
+router.get("/", ProductController.products_get_all);
+
+router.post("/", checkAuth, upload.single("productImage"), ProductController.products_create_product);
+
+router.get("/:productId", ProductController.products_get_product);
+
+router.patch("/:productId", checkAuth, ProductController.products_update_product);
+
+router.delete("/:productId", checkAuth, ProductController.products_delete_product);
+
+module.exports = router;
